976b55678e3a39930667825fff9e1e305722c968
Esteban Canteros <estebanacanteros@gmail.com>
2021-05-26 13:35:22 -0300

    flex property

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import PositionScreen from './src/screens/PositionScreen';
+import FlexBoxScreen from './src/screens/FlexBoxScreen';
+//import PositionScreen from './src/screens/PositionScreen';
 //import DimensionsScreen from './src/screens/DimensionsScreen';
 //import BoxObjectModelScreen from './src/screens/BoxObjectModelScreen';
 //import CounterScreen from './src/screens/CounterScreen';
 
 const App = () => {
   return (
-    <SafeAreaView style={{flex: 1}} >
-      <PositionScreen />
+    <SafeAreaView  style={{flex: 1}}>
+      <FlexBoxScreen />
     </SafeAreaView>
+    //<PositionScreen />
     //<DimensionsScreen />
     //<BoxObjectModelScreen /> 
     //<CounterScreen />
