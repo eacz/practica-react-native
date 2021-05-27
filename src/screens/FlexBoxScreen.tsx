4b1ea4b512da7da5d8ada7b37ae01ba8dc6deedb
Esteban Canteros <estebanacanteros@gmail.com>
2021-05-27 10:20:51 -0300

    flexbox y sus propiedades

--- src/screens/FlexBoxScreen.tsx
+++ src/screens/FlexBoxScreen.tsx
@@ -7,6 +7,27 @@ const FlexBoxScreen = () => {
       <Text style={styles.box1} >Box 1</Text>
       <Text style={styles.box2} >Box 2</Text>
       <Text style={styles.box3} >Box 3</Text>
+      <Text style={styles.box1} >Box 1</Text>
+      <Text style={styles.box2} >Box 2</Text>
+      <Text style={styles.box3} >Box 3</Text>
+      <Text style={styles.box1} >Box 1</Text>
+      <Text style={styles.box2} >Box 2</Text>
+      <Text style={styles.box3} >Box 3</Text>
+      <Text style={styles.box1} >Box 1</Text>
+      <Text style={styles.box2} >Box 2</Text>
+      <Text style={styles.box3} >Box 3</Text>
+      <Text style={styles.box1} >Box 1</Text>
+      <Text style={styles.box2} >Box 2</Text>
+      <Text style={styles.box3} >Box 3</Text>
+      <Text style={styles.box1} >Box 1</Text>
+      <Text style={styles.box2} >Box 2</Text>
+      <Text style={styles.box3} >Box 3</Text>
+      <Text style={styles.box1} >Box 1</Text>
+      <Text style={styles.box2} >Box 2</Text>
+      <Text style={styles.box3} >Box 3</Text>
+      <Text style={styles.box1} >Box 1</Text>
+      <Text style={styles.box2} >Box 2</Text>
+      <Text style={styles.box3} >Box 3</Text>
     </View>
   )
 }
@@ -14,25 +35,32 @@ const FlexBoxScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#28c4d9'
+    flexWrap: 'wrap',
+    //flexDirection: 'row',
+    //justifyContent: 'flex-end',
+    //alignItems: 'flex-end',
+    backgroundColor: '#28c4d9',
+    alignItems: 'flex-start'
   },
   box1: {
-    flex: 3,
+    //flex: 1,
+    //width: '100%',
     borderWidth:2,
     borderColor: 'white',
-    fontSize: 30
+    fontSize: 30,
+    //alignSelf: 'center'
   },
   box2: {
-    flex: 2,
     borderWidth:2,
     borderColor: 'white',
-    fontSize: 30
+    fontSize: 30,
+    //alignSelf: 'flex-start'
   },
   box3: {
-    flex: 1,
     borderWidth:2,
     borderColor: 'white',
-    fontSize: 30
+    fontSize: 30,
+    //alignSelf: 'flex-end'
   }
 });
 
